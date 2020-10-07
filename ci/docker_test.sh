1c73178bf5ff0816e8ff52327d007643ad646400
Ryan <rsthomas@huskers.unl.edu>
2020-10-07 12:18:26 -0500

    Test different sytax for running scipt in docker.

--- ci/docker_test.sh
+++ ci/docker_test.sh
@@ -10,4 +10,4 @@ docker run \
   -w /project/ \
   -v $(pwd):/project/ \
   $IMAGE_NAME \
-  /bin/bash -c "/project/ci/before_script.sh && /project/ci/test.sh"
+  "./project/ci/before_script.sh && ./project/ci/test.sh"
